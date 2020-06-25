1a8009c34848acd624cfa5f695531443c55f015b
Sin-Yao <b01b01012@ntu.edu.tw>
2020-06-25 13:07:56 +0800

    20200625

--- Shiny/Script/Demodata.R
+++ Shiny/Script/Demodata.R
@@ -1,4 +1,8 @@
-demodata <- read.csv("https://sin-yao.github.io/ML-Final-report/Shiny/Data/real_final.csv")
-demodata = demodata[,c(1,2,3,4,5,9,10,11,13,17,18,19,20,21)]
+#demodata <- read.csv("https://sin-yao.github.io/ML-Final-report/Shiny/Data/real_final.csv")
+#demodata = demodata[,c(1,2,3,4,5,9,10,11,13,17,18,19,20,21)]
+#colnames(demodata)<-c("統一編號","名稱","地址","總資本額","核准日","產業別","標準化資本","同產業中成立順序","配發公司",
+#                      "推薦保險一","推薦保險二","公共安全責任險","火災保險","接近嫌惡設施加油站")
+demodata <- read.csv("https://sin-yao.github.io/ML-Final-report/Shiny/Data/NEW2.csv")
+demodata = demodata[,c(2,3,4,5,6,10,11,12,17,18,19,20)]
 colnames(demodata)<-c("統一編號","名稱","地址","總資本額","核准日","產業別","標準化資本","同產業中成立順序","配發公司",
-                      "推薦保險一","推薦保險二","公共安全責任險","火災保險","接近嫌惡設施加油站")
+                      "接近嫌惡設施加油站","公共安全責任險","火災保險")
